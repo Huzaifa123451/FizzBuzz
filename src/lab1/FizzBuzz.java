18041b4ae6ce34a068caea51b31044364dfed51d
unknown <Huzaifa123451>
2025-09-09 22:04:42 -0400

    refactored to extract helper method

--- src/lab1/FizzBuzz.java
+++ src/lab1/FizzBuzz.java
@@ -36,20 +36,25 @@ class FizzBuzz {
         // rewritten using a while loop
         int i = 1;
         while (i < 100) {
-            boolean divisibleBy3 = i % 3 == 0;
-            boolean divisibleBy5 = i % 5 == 0;
-            if (divisibleBy3 && divisibleBy5) {
-                System.out.println("Fizz Buzz");
-            } else if (divisibleBy3) {
-                System.out.println("Fizz");
-            } else if (divisibleBy5) {
-                System.out.println("Buzz");
-            } else {
-                System.out.println(i);
-            }
-
-            i++;
+            i = doFizzbuzz(i);
         }
     }
-}
 
+    private static int doFizzbuzz(int i) {
+        boolean divisibleBy3 = i % 3 == 0;
+        boolean divisibleBy5 = i % 5 == 0;
+        if (divisibleBy3 && divisibleBy5) {
+            System.out.println("Fizz Buzz");
+        } else if (divisibleBy3) {
+            System.out.println("Fizz");
+        } else if (divisibleBy5) {
+            System.out.println("Buzz");
+        } else {
+            System.out.println(i);
+        }
+
+        i++;
+        return i;
+    }
+}
+    
